fa47492f84e701c6f23a1c7df83477ec9f1b7435
itzRoma <romabondarenko03@gmail.com>
2022-09-28 23:07:26 +0300

    division operation added

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+// Addition operation
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+// Substraction operation
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// Multiplication operation
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+// Division operation
+int Calculator::Div (double a, double b)
+{
+    return Mul (a, 1/b);
 }
